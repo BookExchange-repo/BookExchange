508a01ee5b3f6ad9b1fe2ebe055db2424eca88af
Mike <tallinn@gmail.com>
2018-04-01 16:46:30 +0300

    Postregistration forms and their validation added

--- aurelia/src/signup/signup.js
+++ aurelia/src/signup/signup.js
@@ -6,6 +6,56 @@ import { Router } from 'aurelia-router';
 @inject(Router)
 export class SignUp {
   constructor(router) {
-    this.helloMessage = "Sign Up";
+    this.helloMessage = "username";
   }
+
+  attached() {
+
+    $('.ui.form')
+      .form({
+        fields: {
+          firstname: {
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please enter your firstname'
+              }
+            ]
+          },
+          lastname: {
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please enter your lastname'
+              }
+            ]
+          },
+          city: {
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please choose the nearest city to you'
+              }
+            ]
+          },
+          phone: {
+            identifier: 'phone',
+            rules: [
+              {
+                type: 'empty',
+                prompt: 'Please enter your phone number'
+              }
+            ]
+          },
+        }
+      });
+
+  }
+
+  continueButtonPressed () {
+    if( $('.ui.form').form('is valid')) {
+      console.log("test");
+    }
+  }
+
 }

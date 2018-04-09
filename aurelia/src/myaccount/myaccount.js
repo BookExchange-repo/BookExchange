b030a9ae68f3038f7d4f69a40f8884c628fad2d7
Unknown <jertyk@gmail.com>
2018-04-10 01:30:02 +0300

    Edit Mode Version 1

--- aurelia/src/myaccount/myaccount.js
+++ aurelia/src/myaccount/myaccount.js
@@ -7,14 +7,34 @@ let httpClient = new HttpClient();
 
 @inject(Router, Authorization)
 export class MyAccount {
+
+  firstName = "";
+  lastName = "";
+  city = "";
+  phone = "";
+
   constructor(router, authorization) {
     this.router = router;
     this.authorization = authorization;
     this.myAccount = null;
+    this.editMode = false;
+
+    this.cities = null;
   }
 
   attached() {
+    $('.ui.dropdown').dropdown();
+
     this.fetchMyAccount();
+    this.fetchCitiesFromAPI();
+  }
+
+  enableEditMode() {
+    this.editMode = true;
+  }
+
+  disableEditMode() {
+    this.editMode = false;
   }
 
   fetchMyAccount() {
@@ -23,5 +43,36 @@ export class MyAccount {
       .then(data => {
         this.myAccount = JSON.parse(JSON.stringify(data));
       });
+  }
+
+  fetchCitiesFromAPI() {
+    httpClient.fetch('https://bookmarket.online:18081/api/cities/getall')
+      .then(response => response.json())
+      .then(data => {
+        this.cities = data;
+      });
+  }
+
+  updateInformation() {
+    console.log(firstName);
+
+    // if ($('.ui.form').form('is valid')) {
+
+    //   httpClient.fetch('https://bookmarket.online:18081/api/users/update?session=' + this.authorization.getSessionID() + '&fullname=' + 
+    //   this.firstname + " " + this.lastname + '&city=' + this.city + '&phone=' + this.phone)
+    //   .then(function (response) {
+    //     return response.json();
+    //   })
+    //   .then(data => {
+    //     console.log(data);
+    //     if (data.errors.length === 0) {
+    //       this.authorization.deletePostregistrationRequiredSession();
+    //       this.connector.ckeckLoginStatus(); // to refresh data
+    //     } else {
+    //       $('.ui.form').form('add errors', {apiError: 'API error in postregistration'});
+    //     }
+    //   });
+
+    // }
   }
 }

5fde224be6883c7bfd4955e4e22f167fc0a2a0ed
Unknown <jertyk@gmail.com>
2018-04-09 11:22:29 +0300

    MyAccount moqup and getting information from API

--- aurelia/src/myaccount/myaccount.js
+++ aurelia/src/myaccount/myaccount.js
@@ -1,9 +1,23 @@
+import { HttpClient, json } from 'aurelia-fetch-client';
+import { customAttribute, bindable, inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+
+let httpClient = new HttpClient();
+
 export class MyAccount {
   constructor(router) {
-    this.helloMessage = "My account";
+    this.myAccount = null;
   }
 
-  // attached() {
-  //   $('.ui.dropdown').dropdown();
-  // }
+  attached() {
+    this.fetchMyAccount();
+  }
+
+  fetchMyAccount() {
+    httpClient.fetch('https://bookmarket.online:18081/api/users/getinfo?session=vVSicGLfDb2qxU3dUHXwV3Q49NUL29odJM-_yzsntRCvSrSyelW6FdlhIBp6Ld0a')
+      .then(response => response.json())
+      .then(data => {
+        this.myAccount = JSON.parse(JSON.stringify(data));
+      });
+  }
 }

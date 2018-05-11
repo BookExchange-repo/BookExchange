b1c382687f798ee97ee153f94872e79a16183082
Igor Podgainõi <igpodg@ttu.ee>
2018-05-12 00:30:33 +0300

    Estonian language improvements in frontend

--- aurelia/src/app.js
+++ aurelia/src/app.js
@@ -14,14 +14,14 @@ export class App {
 		config.title = 'BookMarket';
 		config.addPipelineStep('authorize', this.authorizestep);
 		config.map([
-			{ route: [''], name: 'home', moduleId: 'home/index', nav: true, title: 'Home' , settings: { roles: [''] }},
-			{ route: 'books', name: 'books', moduleId: 'books/books', nav: true, title: 'All Books' , settings: { roles: [''] }},
-			{ route: 'books/:id', name: 'bookbyid', moduleId: 'book/book', nav: false, title: 'Book' , settings: { roles: [''] }},
-			{ route: 'sellbook', name: 'addbook', moduleId: 'addbook/addbook', nav: true, title: 'Sell Book', settings: { roles: ['reggeduser'] }},
-			{ route: 'signup', name: 'signup', moduleId: 'signup/signup', nav: false, title: 'Sign Up', settings: { roles: ['reggeduser'] }},
-			{ route: 'login', name: 'login', moduleId: 'login/login', nav: true, title: 'Log In', settings: { roles: [''] }},
-			{ route: 'myaccount', name: 'myaccount', moduleId: 'myaccount/myaccount', nav: true, title: 'My Account', settings: { roles: ['reggeduser'] }},
-			{ route: 'history', name: 'history', moduleId: 'history/history', nav: true, title: 'History', settings: { roles: ['reggeduser'] }},
+			{ route: [''], name: 'home', moduleId: 'home/index', nav: true, title: 'Esileht' , settings: { roles: [''] }},
+			{ route: 'books', name: 'books', moduleId: 'books/books', nav: true, title: 'Kõik raamatud' , settings: { roles: [''] }},
+			{ route: 'books/:id', name: 'bookbyid', moduleId: 'book/book', nav: false, title: 'Raamat' , settings: { roles: [''] }},
+			{ route: 'sellbook', name: 'addbook', moduleId: 'addbook/addbook', nav: true, title: 'Müü raamat', settings: { roles: ['reggeduser'] }},
+			{ route: 'signup', name: 'signup', moduleId: 'signup/signup', nav: false, title: 'Registreeri', settings: { roles: ['reggeduser'] }},
+			{ route: 'login', name: 'login', moduleId: 'login/login', nav: true, title: 'Logi sisse', settings: { roles: [''] }},
+			{ route: 'myaccount', name: 'myaccount', moduleId: 'myaccount/myaccount', nav: true, title: 'Minu konto', settings: { roles: ['reggeduser'] }},
+			{ route: 'history', name: 'history', moduleId: 'history/history', nav: true, title: 'Ajalugu', settings: { roles: ['reggeduser'] }},
 		]);
 	}
 }

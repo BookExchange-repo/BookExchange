d1043cfdcc43f5a0439c078318b5e46464cfa740
Igor Podgainõi <igpodg@ttu.ee>
2018-05-16 00:58:47 +0300

    Added search by ISBN

--- src/main/java/ee/ttu/BookExchange/utilities/SizeHelper.java
+++ src/main/java/ee/ttu/BookExchange/utilities/SizeHelper.java
@@ -1,6 +1,9 @@
 package ee.ttu.BookExchange.utilities;
 
 public class SizeHelper {
+    public static final int ISBN_10_LENGTH = 10;
+    public static final int ISBN_13_LENGTH = 13;
+
     public static String mapMarkerSizeToString(int mapMarkerSize) {
         if (mapMarkerSize >= 1 && mapMarkerSize <= 5)
             return "small";
@@ -10,5 +13,18 @@ public class SizeHelper {
             return "big";
         else
             return "none";
+    }
+
+    public static boolean isISBNString(String input) {
+        input = input.trim();
+        int length = input.length();
+        if (length != ISBN_10_LENGTH && length != ISBN_13_LENGTH)
+            return false;
+
+        for (int i = 0; i < length; i++) {
+            if (!Character.isDigit(input.charAt(i)))
+                return false;
+        }
+        return true;
     }
 }
